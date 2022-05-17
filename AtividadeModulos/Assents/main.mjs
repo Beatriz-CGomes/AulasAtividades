def84078030d4c3d749473f617dddbfc6ad69fdb
Beatriz-CGomes <Beatriz.cg15@hotmail.com>
2022-05-17 20:26:08 -0300

    Atividades das aulas JS

---
+++ AtividadeModulos/Assents/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Beatriz", 27));
+console.log(mostraCidade("Beatriz", "São Paulo"));
+console.log(mostraHobby("Beatriz", "Ler", "Estudar", "Ouvir Podcast"));
